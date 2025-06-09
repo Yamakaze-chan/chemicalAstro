70d49160969e98c3de66b74bbcfab9edac18fe3a
Yamakaze-chan <nhatvipmason@gmail.com>
2025-06-09 13:40:39 +0700

    first add

---
+++ src/data/news.ts
@@ -0,0 +1,44 @@
+const posts = [
+  {
+    title: "Lorem Ipsum",
+    href: "#",
+    imageSrc: "https://www.thoughtco.com/thmb/S43kFVYXoxi3gjY6guOrtb6Oe8g=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/chemistry-glassware-56a12a083df78cf772680235.jpg",
+    imageAlt: "Lorem Ipsum",
+    description: "Lorem ipsum dolor sit amet, in sint probo labitur cum. Mei sanctus nonumes ea. Civibus omittam an mei. In praesent expetenda mei.",
+  },
+  {
+    title: "Lorem Ipsum",
+    href: "#",
+    imageSrc: "https://www.thoughtco.com/thmb/S43kFVYXoxi3gjY6guOrtb6Oe8g=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/chemistry-glassware-56a12a083df78cf772680235.jpg",
+    imageAlt: "Lorem Ipsum",
+    description: "Lorem ipsum dolor sit amet, in sint probo labitur cum. Mei sanctus nonumes ea. Civibus omittam an mei. In praesent expetenda mei.",
+  },
+  {
+    title: "Lorem Ipsum",
+    href: "#",
+    imageSrc: "https://www.thoughtco.com/thmb/S43kFVYXoxi3gjY6guOrtb6Oe8g=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/chemistry-glassware-56a12a083df78cf772680235.jpg",
+    imageAlt: "Lorem Ipsum",
+    description: "Lorem ipsum dolor sit amet, in sint probo labitur cum. Mei sanctus nonumes ea. Civibus omittam an mei. In praesent expetenda mei.",
+  },
+  {
+    title: "Lorem Ipsum",
+    href: "#",
+    imageSrc: "https://www.thoughtco.com/thmb/S43kFVYXoxi3gjY6guOrtb6Oe8g=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/chemistry-glassware-56a12a083df78cf772680235.jpg",
+    imageAlt: "Lorem Ipsum",
+    description: "Lorem ipsum dolor sit amet, in sint probo labitur cum. Mei sanctus nonumes ea. Civibus omittam an mei. In praesent expetenda mei.",
+  },
+  {
+    title: "Lorem Ipsum",
+    href: "#",
+    imageSrc: "https://www.thoughtco.com/thmb/S43kFVYXoxi3gjY6guOrtb6Oe8g=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/chemistry-glassware-56a12a083df78cf772680235.jpg",
+    imageAlt: "Lorem Ipsum",
+    description: "Lorem ipsum dolor sit amet, in sint probo labitur cum. Mei sanctus nonumes ea. Civibus omittam an mei. In praesent expetenda mei.",
+  },
+  {
+    title: "Lorem Ipsum",
+    href: "#",
+    imageSrc: "https://www.thoughtco.com/thmb/S43kFVYXoxi3gjY6guOrtb6Oe8g=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/chemistry-glassware-56a12a083df78cf772680235.jpg",
+    imageAlt: "Lorem Ipsum",
+    description: "Lorem ipsum dolor sit amet, in sint probo labitur cum. Mei sanctus nonumes ea. Civibus omittam an mei. In praesent expetenda mei.",
+  }
+];
---
+++ src/data/products.ts
@@ -0,0 +1,61 @@
+export const products = [
+  {
+    id: 1,
+    title: "Trust & Co.",
+    image:
+      "https://nailknowledge.org/wp-content/uploads/2021/04/chemical-1024x683.jpg",
+    description:
+      "Fill out the form and the algorithm will offer the right team of experts",
+    tags: [
+      { label: "branding", color: "#d3b19a" },
+      { label: "packaging", color: "#70b3b1" },
+    ],
+  },
+  {
+    id: 2,
+    title: "BioChem Corp.",
+    image: "https://images.unsplash.com/photo-1581090700227-1e8e8e4b5a9e",
+    description: "Innovative solutions for chemical packaging and safety",
+    tags: [
+      { label: "research", color: "#a1c4fd" },
+      { label: "logistics", color: "#fcb69f" },
+    ],
+  },
+  {
+    id: 3,
+    title: "Trust & Co.",
+    image:
+      "https://nailknowledge.org/wp-content/uploads/2021/04/chemical-1024x683.jpg",
+    description:
+      "Fill out the form and the algorithm will offer the right team of experts",
+    tags: [
+      { label: "branding", color: "#d3b19a" },
+      { label: "packaging", color: "#70b3b1" },
+    ],
+  },
+  {
+    id: 4,
+    title: "Trust & Co.",
+    image:
+      "https://nailknowledge.org/wp-content/uploads/2021/04/chemical-1024x683.jpg",
+    description:
+      "Fill out the form and the algorithm will offer the right team of experts",
+    tags: [
+      { label: "branding", color: "#d3b19a" },
+      { label: "packaging", color: "#70b3b1" },
+    ],
+  },
+  {
+    id: 5,
+    title: "Trust & Co.",
+    image:
+      "https://nailknowledge.org/wp-content/uploads/2021/04/chemical-1024x683.jpg",
+    description:
+      "Fill out the form and the algorithm will offer the right team of experts",
+    tags: [
+      { label: "branding", color: "#d3b19a" },
+      { label: "packaging", color: "#70b3b1" },
+    ],
+  },
+  // Add more products as needed
+];
