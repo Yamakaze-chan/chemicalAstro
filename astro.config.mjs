3775f9e397354d0cbcf29e74a8e45214275cafb8
Yamakaze-chan <nhatvipmason@gmail.com>
2025-06-19 14:58:56 +0700

    add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import dotenv from "dotenv";
-dotenv.config();
+import cloudflare from "@astrojs/cloudflare";
+// import dotenv from "dotenv";
+// dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
